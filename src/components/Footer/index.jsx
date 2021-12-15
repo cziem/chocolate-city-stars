0e1d7b204f20d73a9ae8ca7c9b5ce210ce346eab
George Favour C <phavorsparks@gmail.com>
2021-12-15 20:41:36 +0100

    style the footer section

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,18 @@
+import { Box, Typography } from "@mui/material"
 import React from "react"
+import "./footer.styles.scss"
 
 const Footer = () => {
-  return <div>choco 2021 &copy;</div>
+  return (
+    <Box
+      sx={{ flexGrow: 1, display: "flex", justifyContent: "center" }}
+      className="footer-main"
+    >
+      <Typography>
+        <b>Chocity</b>Stars 2021 &copy;
+      </Typography>
+    </Box>
+  )
 }
 
 export default Footer
